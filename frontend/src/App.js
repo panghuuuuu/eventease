6667cfedc4c1695c53bff41a518a5c799ed66fd9
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2024-03-12 19:11:03 +0800

    fixed login and signip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import history from "./history";
 import "./stylesheets/App.css";
 
+import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 const App = () => (
   <div>
-    <Signup />
+    <Login />
   </div>
 );
 
