fe64ad5cf79a248b91183e98264b5f7ee48a928c
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2024-03-21 09:26:51 +0800

    added components in browse page

--- frontend/src/pages/BrowsePage.js
+++ frontend/src/pages/BrowsePage.js
@@ -23,7 +23,34 @@ const BrowsePage = () => {
             <li>Costumes</li>
             <li>Makeup Artist</li>
           </ul>
-          <h2>Filters</h2>
+          <div className="filters_container">
+            <h2>Filters</h2>
+            <div className="location_filter">
+              <p className>By Location</p>
+              <select className="dropdown" id="dropdown" name="location">
+                <option value="Philippines">Philippines</option>
+              </select>
+            </div>
+            <div className="budget_filter">
+              <p className="input_label">By Budget</p>
+              <div className="input">
+                <input type="float" name="budget" placeholder="PHP" />
+              </div>
+              <div className="slidecontainer">
+                <input
+                  type="range"
+                  min="1"
+                  max="250"
+                  className="slider"
+                  id="myRange"
+                />
+                <div className="range_text">
+                  <p>0 PHP</p>
+                  <p>500000+ PHP</p>{" "}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="browse_panel">
           {Object.entries(Services).map(
