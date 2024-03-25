2d2d60a817427efd81de1b43b81a20e605f59d1c
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2024-03-25 22:41:07 +0800

    redirect to myevents after login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,14 +1,38 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import axiosInstance from "../axiosApi";
 import signupHeader from "../assets/logo_header.png";
 import "../stylesheets/signup.css";
+
 export default function Login() {
   const [formData, setFormData] = useState({
     username: "",
     password: "",
   });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      try {
+        const response = await axiosInstance.get("/api/checkLoggedIn");
+        if (response.status === 200) {
+          setIsLoggedIn(true);
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+    checkLoggedIn();
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/myevents");
+    }
+  }, [isLoggedIn, navigate]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -16,11 +40,19 @@ export default function Login() {
   };
 
   const submitForm = async () => {
-    console.log(formData);
+    setLoading(true);
+    setError(null);
+
     try {
-      await axiosInstance.post("/api/login/", formData);
+      const response = await axiosInstance.post("/api/login/", formData);
+      if (response.status === 200) {
+        setIsLoggedIn(true);
+      }
     } catch (error) {
+      setError("Invalid username or password.");
       console.error("Error:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -33,7 +65,6 @@ export default function Login() {
         </div>
         <div className="signup_fields">
           <h3>Login</h3>
-
           <div className="input">
             <p className="input_label">
               Username<span>*</span>
